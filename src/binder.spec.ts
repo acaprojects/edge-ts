36a6ae59f154ba330aecc4ea7d27f28b4998d575
Kim Burgess <kim@acaprojects.com>
2017-06-06 16:43:34 +1000

    Update tests

--- src/binder.spec.ts
+++ src/binder.spec.ts
@@ -2,13 +2,11 @@ import { sync, async } from './binder';
 import 'mocha';
 import { expect } from 'chai';
 
+const id = `async (dynamic input) => { return input; }`;
+
 describe('sync()', () => {
     it('support binding to CLR that\'s compiled on the fly', () => {
-        const identity = sync<number, number>({
-            source: 'src/bindings/TestBinding.cs',
-            typeName: 'Test.TestBinding',
-            methodName: 'Identity'
-        });
+        const identity = sync<number, number>(id);
 
         const input = Math.random();
         const result = identity(input);
@@ -16,32 +14,12 @@ describe('sync()', () => {
         expect(result).to.equal(input);
     });
 
-    it('supports binding to precompiled assemblies', () => {
-        if (process.platform === 'win32') {
-            const identity = sync<number, number>({
-                assemblyFile: 'lib/native/win32/SkypeClient.dll',
-                typeName: 'Test.TestBinding',
-                methodName: 'Identity'
-            });
-
-            const input = Math.random();
-            const result = identity(input);
-
-            expect(result).to.equal(input);
-        } else {
-            this.skip();
-        }
-    });
+    // TODO implement tests for pre-compiled sync bindings
 });
 
 describe('async()', () => {
-
     it('support binding to CLR that\'s compiled on the fly', (done) => {
-        const identity = async<number, number>({
-            source: 'src/bindings/TestBinding.cs',
-            typeName: 'Test.TestBinding',
-            methodName: 'Identity'
-        });
+        const identity = async<number, number>(id);
 
         const input = Math.random();
 
@@ -56,27 +34,5 @@ describe('async()', () => {
             .catch(done);
     });
 
-    it('supports binding to precompiled assemblies', (done) => {
-        if (process.platform === 'win32') {
-            const identity = async<number, number>({
-                assemblyFile: 'lib/native/win32/SkypeClient.dll',
-                typeName: 'Test.TestBinding',
-                methodName: 'Identity'
-            });
-
-            const input = Math.random();
-
-            identity(input)
-                .then((result: any) => {
-                    if (result === input) {
-                        done();
-                    } else {
-                        done('Unexpected result returned');
-                    }
-                })
-                .catch(done);
-        } else {
-            this.skip();
-        }
-    });
+    // TODO implement test for precompiled async bindings
 });
